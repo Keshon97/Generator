809e207d990e06a0ddd6390bc4b2f1ec2ce58879
Keshon Royster <k.royster512@gmail.com>
2022-11-03 15:39:07 -0500

    reformatted index.html and created style.css

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const template = require('./src/html-template');
 
-
+//imported classes
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
@@ -24,7 +25,6 @@ function employees() {
     }
     ])
 
-    
     .then(({role, name, id, email}) => {
       if (role === 'Manager') {
         return inquirer.prompt 
@@ -56,8 +56,8 @@ function employees() {
             choices: ['Yes', 'No']
           }
         ])
-        .then (({input, nextEmployee}) => {
-          const manager = new Manager (input.name, input.id, input.email, input.officeNumber);
+        .then ((input, nextEmployee) => {
+          const manager = new Manager (input.name, input.id, input.email, input.officeNumber)
           //adds manager data to myTeam array
           myTeam.push(manager);
           //will restart function if user wants to add another team member
@@ -149,23 +149,3 @@ function employees() {
 
 employees();
 
-//Create html format
-//add validation if I want for user input to ensure it's in proper format  
-
-
-{/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>  */}
